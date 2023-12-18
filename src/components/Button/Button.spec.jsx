548b0cc68fd23146d88e87cf177c2411deea430a
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-12-18 14:34:36 -0300

    add onClick button test

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Button } from '.';
 
 describe('<Button />', () => {
@@ -7,5 +7,14 @@ describe('<Button />', () => {
 
     const button = screen.getByRole('button', { name: /load more/i });
     expect(button).toBeInTheDocument();
+  });
+
+  it('should call function on button click', () => {
+    const fn = jest.fn();
+    render(<Button text='Load more' handleClick={fn} />);
+
+    const button = screen.getByRole('button', { name: /load more/i });
+    fireEvent.click(button);
+    expect(fn).toHaveBeenCalledTimes(1);
   });
 });
