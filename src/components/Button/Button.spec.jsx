d24f6f674f6f8bc620a8ebb7511010ec3eb91f92
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-12-18 14:47:34 -0300

    add button enabled/disabled tests

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -17,4 +17,18 @@ describe('<Button />', () => {
     fireEvent.click(button);
     expect(fn).toHaveBeenCalledTimes(1);
   });
+
+  it('should be disabled when disabled is true', () => {
+    render(<Button text='Load more' disabled={true} />);
+
+    const button = screen.getByRole('button', { name: /load more/i });
+    expect(button).toBeDisabled();
+  });
+
+  it('should be enabled when disabled is false', () => {
+    render(<Button text='Load more' disabled={false} />);
+
+    const button = screen.getByRole('button', { name: /load more/i });
+    expect(button).toBeEnabled();
+  });
 });
