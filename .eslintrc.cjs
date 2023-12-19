e4bcc470402dd1aeeabd60886beb1116c5c87488
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-12-19 13:22:29 -0300

    add button propTypes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -32,6 +32,6 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 'off',
     'no-unused-vars': 'warn',
-    'react/prop-types': 'off',
+    //'react/prop-types': 'off',
   },
 };
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,20 @@
+import PropTypes from 'prop-types';
 import './styles.scss';
 
-export const Button = ({ handleClick, text, disabled }) => {
+export const Button = ({ handleClick, text, disabled = false }) => {
   return (
     <button className='button' onClick={handleClick} disabled={disabled}>
       {text}
     </button>
   );
+};
+
+Button.defaultProps = {
+  disabled: false,
+};
+
+Button.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
 };
